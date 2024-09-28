f013aa18297a33a8c03341791607ceca64622d29
kimtaegyuu <andrew0721@naver.com>
2024-09-28 20:50:54 +0900

    Update generate_seqsim.sh

--- code/generate_seqsim.sh
+++ code/generate_seqsim.sh
@@ -4,7 +4,7 @@ query=$1
 name=${query:0:-6}
 
 
-blastp -query $1 -db blast_db/${name}_positive -evalue 10 -out blast_db/${name}_positive
-blastp -query $1 -db blast_db/${name}_negative -evalue 10 -out blast_db/${name}_negative
+blastp -query $1 -db blast_db/positive -evalue 10 -out blast_db/positive
+blastp -query $1 -db blast_db/negative -evalue 10 -out blast_db/negative
 
 python3 seqsim.py --query ${name}
