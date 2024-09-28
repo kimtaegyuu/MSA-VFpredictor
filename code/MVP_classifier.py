f98bf5224f997ab44c46771ce46dd152adb119c9
kimtaegyuu <andrew0721@naver.com>
2024-09-28 21:21:22 +0900

    Update MVP_classifier.py

--- code/MVP_classifier.py
+++ code/MVP_classifier.py
@@ -1,11 +1,14 @@
 from torch import nn
 import torch as th
 import torch.nn.functional as F
+import torch
 
 class MVP_classifier(nn.Module):
     def __init__(self, batch_size = 1):
-        super(MVP_classifier, self).__init__()     
-        self.ffc1 = nn.Linear(1024*768+2, 4096)
+        super(MVP_classifier, self).__init__()
+        self.batch_size = batch_size
+        self.fc_mat = nn.Linear(1089, 1)
+        self.ffc1 = nn.Linear(20*768+2, 4096)
         self.ffc2 = nn.Linear(4096, 1024)
         self.ffc3 = nn.Linear(1024, 512)
         self.ffc4 = nn.Linear(512, 2)
@@ -18,6 +21,6 @@ class MVP_classifier(nn.Module):
         x = F.leaky_relu(self.ffc2(x))
         x = self.dropout(x)
         x = F.leaky_relu(self.ffc3(x))
-        x = self.dropout(x)
+        x = self.dropout(x)     
         x = F.softmax(self.ffc4(x),dim=1)
         return x
